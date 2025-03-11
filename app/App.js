011c3e2e002139790da1d25db3390cc143e99009
IssahBen <benedictissah@gmail.com>
2025-03-11 09:38:22 -0400

    rendering error fixed

--- app/App.js
+++ app/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { verifyInstallation } from 'nativewind';
 import "../global.css"
 import MyTabs from './components/Shared/Tab';
 export default function App() {
+  verifyInstallation();
   return( 
     <NavigationContainer>
      <MyTabs/>
